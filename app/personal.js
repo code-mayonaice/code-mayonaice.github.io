a557007bbbe9f5d8ae9f79760a6eb99ee9b1bc41
code-mayonaice <revillanhoel@gmail.com>
2021-03-21 23:25:53 +0800

    3/21/

--- app/personal.js
+++ app/personal.js
@@ -1,104 +1,82 @@
-const profilearrow = document.querySelector('.arrow');
-const profilepopup = document.querySelector('.popup-profile');
-const projectpopup = document.querySelector('.popup-projects');
-const projectarrow = document.querySelector('.projects-arrow');
-const skillsarrow = document.querySelector('.skills-arrow');
-const contactarrow = document.querySelector('.contact-arrow');
-const popupskills = document.querySelector('.popup-skills');
-const popupcontact = document.querySelector('.popup-contact');
-const homearrow = document.querySelector('.home-arrow');
-const resumeholder = document.querySelector('.resume-holder');
-const profilenav = document.querySelector('.profilenav');
-const projectnav = document.querySelector('.projectnav');
-const skillnav = document.querySelector('.skillnav');
-const contactnav = document.querySelector('.contactnav');
-const profileremover = document.querySelector('.remover');
-const projectremover = document.querySelector('.cancel');
-const skillsremover = document.querySelector('.skillsremover');
-const contactremover = document.querySelector('.contact-remover');
-const resumeremover = document.querySelector('.resume-remover');
-const hamburger = document.querySelector('.hamburger');
-const hamburgernav = document.querySelector('.responsive-navbar-container');
+
+function profile(){
+ 
+  const profilepopup = document.querySelector('.popup-profile');
+  profilepopup.classList.add('popup-active');
+
+}
 
 
+function projects(){
+
+  const projectpopup = document.querySelector('.popup-projects');
+  projectpopup.classList.add('popup-active');
+
+}
+
+function skills(){
+
+  const skillstpopup = document.querySelector('.popup-skills');
+  skillstpopup.classList.add('popup-active');
+
+}
 
 
-profilearrow.addEventListener('click',function(){
-profilepopup.classList.add('popup-active');
+function contact(){
 
-});
+  const contactpopup = document.querySelector('.popup-contact');
+  contactpopup.classList.add('popup-active');
 
-projectarrow.addEventListener('click',function(){
-     projectpopup.classList.add('popup-active');
-});
+}
 
-  skillsarrow.addEventListener('click',function(){
-    popupskills.classList.add('popup-active');
-  });
+function home(){
 
-contactarrow.addEventListener('click',function(){
-  popupcontact.classList.add('popup-active');
-});
 
-  profilenav.addEventListener('click',function(){
-    profilepopup.classList.add('popup-active');
-  });
-  
-  projectnav.addEventListener('click',function(){
-    projectpopup.classList.add('popup-active');
-  });
-  
+  const contactpopup = document.querySelector('.popup-contact');
+  const skillstpopup = document.querySelector('.popup-skills');
+  const projectpopup = document.querySelector('.popup-projects');
+  const profilepopup = document.querySelector('.popup-profile');
 
-  skillnav .addEventListener('click',function(){
-    popupskills.classList.add('popup-active');
-  });
+   contactpopup.classList.remove('popup-active');
+   skillstpopup.classList.remove('popup-active');
+   projectpopup.classList.remove('popup-active');
+   profilepopup.classList.remove('popup-active');
 
-  contactnav.addEventListener('click',function(){
-    popupcontact.classList.add('popup-active');
-  });
+}
 
-contactremover.addEventListener('click',function(){
-  popupcontact.classList.remove('popup-active');
-
-});
-
-skillsremover.addEventListener('click',function(){
-  popupskills.classList.remove('popup-active');
-})
-
-projectremover.addEventListener('click',function(){
-  projectpopup.classList.remove('popup-active');
-});
-
-profileremover.addEventListener('click',function(){
-  profilepopup.classList.remove('popup-active');
-
-});
-
-homearrow.addEventListener('click',function(){
-  popupcontact.classList.remove('popup-active');
-  popupskills.classList.remove('popup-active');
-  projectpopup.classList.remove('popup-active');
-  profilepopup.classList.remove('popup-active');
-});
-
-resumeremover.addEventListener('click',function(){
-  resumeholder.classList.remove('resume-active');
-  
-});
 
 function resume(){
+
+  const resumeholder = document.querySelector('.resume-holder');
   resumeholder.classList.add('resume-active');
   
 }
 
-
-hamburger.addEventListener('click',function(){
-
-  hamburgernav.classList.toggle('responisve-navbar-active');
+function remove(){
+ 
+  const profilepopup = document.querySelector('.popup-profile');
+  const contactpopup = document.querySelector('.popup-contact');
+  const skillstpopup = document.querySelector('.popup-skills');
+  const projectpopup = document.querySelector('.popup-projects');
   
-});
 
+  profilepopup.classList.remove('popup-active'); 
+  skillstpopup.classList.remove('popup-active'); 
+  projectpopup.classList.remove('popup-active'); 
+  contactpopup.classList.remove('popup-active'); 
+ 
+
+}
+
+function hamburger(){
+  const hamburgernav = document.querySelector('.responsive-navbar-container');
+  hamburgernav.classList.toggle('responisve-navbar-active');
+}
+
+function resumeremove(){
+  const resumeholder = document.querySelector('.resume-holder');
+  resumeholder.classList.remove('resume-active');
+}
 
 
 
